de203ad283a25f4f0cf93d9b32c28cf5b5b4fe05
shaadomanthra <shaadomanthra@yahoo.com>
2021-10-18 16:23:12 +0530

    some changes

--- resources/views/appl/test/attempt/blocks/mcq.blade.php
+++ resources/views/appl/test/attempt/blocks/mcq.blade.php
@@ -31,5 +31,10 @@
   @include('appl.test.attempt.layouts.gre_maq')
 @endif
 
+@if($m->layout == 'gre_numeric')
+<h5><b> Enter your answer</b></h5>
+  <input class='input_ {{$m->qno}} form-control w-25' type="text" name="{{$m->qno}}"  data-id="{{$m->qno}}" value=""/>
+@endif
+
 </div>            
 @endforeach
