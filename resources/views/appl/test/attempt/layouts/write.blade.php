9561fbdeabfb6fcf0b0b78d110a84dc62168c2c0
shaadomanthra <shaadomanthra@yahoo.com>
2022-06-08 16:28:54 +0530

    some changes

--- resources/views/appl/test/attempt/layouts/write.blade.php
+++ resources/views/appl/test/attempt/layouts/write.blade.php
@@ -1,10 +1,15 @@
 
-@if($f->label ) <div class="pt-3 pb-5 text-center duo-heading"><b>Respond to the question in at least 50 words</b></div> @else
+@if($f->label ) <div class="pt-3 pb-5 text-center duo-heading"><b>Respond to the question in at least 50 words</b></div> 
+@elseif($f->prefix)
+<div class="pt-3 pb-5 duo-heading">{!!$f->prefix!!}</div>
+@else
 <div class="pt-3 pb-5 text-center duo-heading"><b>Write one or more sentences that describe the image</b></div>
 @endif
 <div class="row question ">
-<div class="col-12 @if($f->label) col-md-5 @else col-md-3 @endif">
-@if($f->label ) <div class="h5 mt-3">{{$f->label }}</div>  @else
+<div class="col-12 @if($f->label) col-md-5 @elseif($f->prefix) @else col-md-3 @endif">
+@if($f->label ) <div class="h5 mt-3">{{$f->label }}</div>  
+@elseif($f->prefix)
+@else
 <div class="text-center">
 <div class="img_container float-md-right mb-3" style="">
 	<img src="{{ asset('/storage/extracts/'.$f->id.'_q_300.jpg') }}" class="w-100" style="border-radius:5px;">
@@ -15,10 +20,12 @@
   <div class="col-12 col-md">
   	<div class="mb-4 ">
 	
+
 	<textarea class="summernote4" name="{{$f->qno}}" data-id="{{$f->qno}}" ></textarea>
-	@if($f->label ) 
+	@if($f->label || $f->prefix) 
 	<div class=" mt-1 text-secondary"><span class="f-word-count wc_{{$f->qno}}">0</span> words</div>
 	@endif
+
 </div>
   </div>
 </div>
