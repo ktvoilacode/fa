7fe2af58ba197003d86e5d4d9ea7aa0533abc045
shaadomanthra <shaadomanthra@yahoo.com>
2024-01-21 11:56:41 +0530

    some changes

--- resources/views/appl/test/attempt/blocks/write2.blade.php
+++ resources/views/appl/test/attempt/blocks/write2.blade.php
@@ -48,7 +48,7 @@
 @else
 
   @if(request()->get('source'))
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
