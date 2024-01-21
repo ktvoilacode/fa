d7aaebd74776c897760b11f91376c6e3af4963f3
shaadomanthra <shaadomanthra@yahoo.com>
2024-01-21 11:52:38 +0530

    some changes

--- resources/views/appl/test/attempt/blocks/write2.blade.php
+++ resources/views/appl/test/attempt/blocks/write2.blade.php
@@ -47,30 +47,70 @@
 </div>
 @else
 
-<div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Login Now</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Only logged in users can submit the test.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <a href="{{ route('login')}}">
-        <button type="button" class="btn btn-success">Login</button>
-        </a>
-        <a href="{{ route('register')}}">
-        <button type="button" class="btn btn-primary">Register</button>
-        </a>
+  @if($test->status==3 || request()->get('source'))
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Test Submission</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Select the below checkbox to confirm your test submission</p>
+         
+          <ul>
+          <li>Tasks once submitted cannot be changed.</li>
+          <li>Evaluation will take at least <span class="badge badge-warning">&nbsp;2&nbsp;</span> working days.</li>
+          <li> Tasks with plagiarism shall <u>not</u> be evaluated but will count as a task.</li>
+          <li>One-to-One sessions for writing will be based on any <span class="badge badge-warning">&nbsp;1&nbsp;</span> task submitted here. </li>
+          <li>Scores are indicative and are purely for training purposes.</li> 
+        </ul>
+        <div class="p-3 rounded bg-warning">
+       <input type="checkbox" name="accept" class="accept" > &nbsp;<b>I/We accept the terms</b></input>
+     </div>
+        </div>
+        <div class="modal-footer">
+          <input type="hidden" name="name" value="{{ request()->get('name') }}">
+          <input type="hidden" name="username" value="{{ request()->get('username') }}">
+          <input type="hidden" name="id" value="{{ request()->get('id') }}">
+          <input type="hidden" name="uri" value="{{ request()->get('uri') }}">
+          <input type="hidden" name="source" value="{{ request()->get('source') }}"><input type="hidden" name="source_product" value="{{ request()->get('product') }}">
+          <input type="hidden" name="private" value="{{ request()->get('private') }}">
+          <input type="hidden" name="open" value="1">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+          <button type="submit" id="submit_btn" class="btn btn-success">Confirm Submission</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
+  @else
+  <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Login Now</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Only logged in users can submit the test.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <a href="{{ route('login')}}">
+          <button type="button" class="btn btn-success">Login</button>
+          </a>
+          <a href="{{ route('register')}}">
+          <button type="button" class="btn btn-primary">Register</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endif
 
 
 @endif
