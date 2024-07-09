be306aeeaef861c7df5a937c19eb7ee2feaf97a2
Krishna Teja <kt@voilacode.com>
2024-07-09 23:01:33 +0530

    some changes

--- app/Http/Controllers/Test/McqController.php
+++ app/Http/Controllers/Test/McqController.php
@@ -16,12 +16,13 @@ class McqController extends Controller
         The MCQ Controller
     */
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->app      =   'test';
         $this->module   =   'mcq';
-        if(request()->route('test')){
+        if (request()->route('test')) {
             $this->test = Test::find(request()->route('test'));
-        } 
+        }
     }
 
     /**
@@ -29,46 +30,46 @@ class McqController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Obj $obj,Request $request)
+    public function index(Obj $obj, Request $request)
     {
         $this->authorize('view', $obj);
 
         $search = $request->search;
         $item = $request->item;
         $sid = $request->get('section_id');
-        
-        $objs = $obj->where('test_id',$this->test->id)->where(function ($query) use ($item) {
-                $query->where('question','LIKE',"%{$item}%")
-                    ->orWhere('a','LIKE',"%{$item}%")
-                     ->orWhere('b','LIKE',"%{$item}%")
-                    ->orWhere('c','LIKE',"%{$item}%");
-                    })
-                    ->orderByRaw('CONVERT(qno, SIGNED) asc')
-                    ->paginate(120);  
-        $sections = Section::whereIn('id',$objs->pluck('section_id')->toArray())->get()->keyBy('id');
+
+        $objs = $obj->where('test_id', $this->test->id)->where(function ($query) use ($item) {
+            $query->where('question', 'LIKE', "%{$item}%")
+                ->orWhere('a', 'LIKE', "%{$item}%")
+                ->orWhere('b', 'LIKE', "%{$item}%")
+                ->orWhere('c', 'LIKE', "%{$item}%");
+        })
+            ->orderByRaw('CONVERT(qno, SIGNED) asc')
+            ->paginate(120);
+        $sections = Section::whereIn('id', $objs->pluck('section_id')->toArray())->get()->keyBy('id');
         $scounter = $objs->groupBy('section_id');
 
-        if($sid){
-            $objs = $obj->where('test_id',$this->test->id)->where(function ($query) use ($item,$sid) {
-                $query->where('question','LIKE',"%{$item}%")
-                    ->orWhere('a','LIKE',"%{$item}%")
-                     ->orWhere('b','LIKE',"%{$item}%")
-                    ->orWhere('c','LIKE',"%{$item}%");
-                    })
-                    ->where('section_id',$sid)
-                    ->orderByRaw('CONVERT(qno, SIGNED) asc')
-                    ->paginate(120);  
+        if ($sid) {
+            $objs = $obj->where('test_id', $this->test->id)->where(function ($query) use ($item, $sid) {
+                $query->where('question', 'LIKE', "%{$item}%")
+                    ->orWhere('a', 'LIKE', "%{$item}%")
+                    ->orWhere('b', 'LIKE', "%{$item}%")
+                    ->orWhere('c', 'LIKE', "%{$item}%");
+            })
+                ->where('section_id', $sid)
+                ->orderByRaw('CONVERT(qno, SIGNED) asc')
+                ->paginate(120);
         }
 
-        $view = $search ? 'list': 'index';
+        $view = $search ? 'list' : 'index';
 
-        return view('appl.'.$this->app.'.'.$this->module.'.'.$view)
-                ->with('objs',$objs)
-                ->with('obj',$obj)
-                ->with('sections',$sections)
-                ->with('scounter',$scounter)
-                ->with('try',true)
-                ->with('app',$this);
+        return view('appl.' . $this->app . '.' . $this->module . '.' . $view)
+            ->with('objs', $objs)
+            ->with('obj', $obj)
+            ->with('sections', $sections)
+            ->with('scounter', $scounter)
+            ->with('try', true)
+            ->with('app', $this);
     }
 
     public function layout()
@@ -77,10 +78,10 @@ class McqController extends Controller
         $this->authorize('create', $obj);
         $test_id = $this->test->id;
 
-        return view('appl.'.$this->app.'.'.$this->module.'.layout')
-                ->with('stub','Create')
-                ->with('obj',$obj)
-                ->with('app',$this);
+        return view('appl.' . $this->app . '.' . $this->module . '.layout')
+            ->with('stub', 'Create')
+            ->with('obj', $obj)
+            ->with('app', $this);
     }
 
     /**
@@ -93,36 +94,34 @@ class McqController extends Controller
         $obj = new Obj();
         $this->authorize('create', $obj);
 
-        $this->qno =1;
+        $this->qno = 1;
         $this->sno = 1;
         /* add the serial number */
-        if(Obj::orderBy('id','desc')->where('test_id',$this->test->id)->first()){
-            $m = Obj::orderBy('id','desc')->where('test_id',$this->test->id)->first();
-            if(is_numeric($m->qno)){
-                 $this->qno = $m->qno +1;
-                $this->sno = $m->sno+1;
+        if (Obj::orderBy('id', 'desc')->where('test_id', $this->test->id)->first()) {
+            $m = Obj::orderBy('id', 'desc')->where('test_id', $this->test->id)->first();
+            if (is_numeric($m->qno)) {
+                $this->qno = $m->qno + 1;
+                $this->sno = $m->sno + 1;
             }
-           
-        }
-        else{
-            $this->qno =1;
+        } else {
+            $this->qno = 1;
             $this->sno = 1;
         }
 
-        if(request()->get('layout'))
+        if (request()->get('layout'))
             $obj->layout = request()->get('layout');
 
-        $extracts = Extract::where('test_id',$this->test->id)->get();
-        $sections = Section::where('test_id',$this->test->id)->get();
+        $extracts = Extract::where('test_id', $this->test->id)->get();
+        $sections = Section::where('test_id', $this->test->id)->get();
         $tags = Tag::all();
-        return view('appl.'.$this->app.'.'.$this->module.'.createedit')
-                ->with('stub','Create')
-                ->with('obj',$obj)
-                ->with('editor',true)
-                ->with('extracts',$extracts)
-                ->with('sections',$sections)
-                ->with('tags',$tags)
-                ->with('app',$this);
+        return view('appl.' . $this->app . '.' . $this->module . '.createedit')
+            ->with('stub', 'Create')
+            ->with('obj', $obj)
+            ->with('editor', true)
+            ->with('extracts', $extracts)
+            ->with('sections', $sections)
+            ->with('tags', $tags)
+            ->with('app', $this);
     }
 
     /**
@@ -133,83 +132,82 @@ class McqController extends Controller
      */
     public function store(Obj $obj, Request $request)
     {
-        try{
+        try {
             $user = \auth::user();
 
             $request->session()->put('extract_id', $request->get('extract_id'));
-            
+
             $this->imageupdater($request);
-            
-            
+
+
             // for multi answer
             $answers = $request->get('answers');
-            if($answers){
+            if ($answers) {
                 $answer = implode(', ', $answers);
                 /* merge the updated data in request */
-                $request->merge(['answer'=>$answer]);
-
+                $request->merge(['answer' => $answer]);
             }
-            
-            $request->merge(['qno'=>str_replace(" ","",$request->qno)]);
+
+            $request->merge(['qno' => str_replace(" ", "", $request->qno)]);
             /* create a new entry */
             $obj = $obj->create($request->except(['tags']));
 
             // attach the tags
             $tags = $request->get('tags');
-            if($tags)
-            foreach($tags as $tag){
-                $obj->tags()->attach($tag);
-            }
+            if ($tags)
+                foreach ($tags as $tag) {
+                    $obj->tags()->attach($tag);
+                }
 
-            
+
             //update extract
-            if($request->get('extract_id'))
-            {
-                if($obj->extract)
-                $obj->extract->extract_update($obj->qno);
+            if ($request->get('extract_id')) {
+                if ($obj->extract)
+                    $obj->extract->extract_update($obj->qno);
             }
 
-            flash('A new ('.$this->app.'/'.$this->module.') item is created!')->success();
-            return redirect()->route($this->module.'.index',[$this->test->id]);
-        }
-        catch (QueryException $e){
-           $error_code = $e->errorInfo[1];
-            if($error_code == 1062){
+            flash('A new (' . $this->app . '/' . $this->module . ') item is created!')->success();
+            return redirect()->route($this->module . '.index', [$this->test->id]);
+        } catch (QueryException $e) {
+            $error_code = $e->errorInfo[1];
+            if ($error_code == 1062) {
                 flash('Some error in Creating the record')->error();
-                 return redirect()->back()->withInput();;
+                return redirect()->back()->withInput();;
             }
         }
     }
 
-    public function imageupdater($request){
+    public function imageupdater($request)
+    {
 
         $user = \auth::user();
-        $options =['question','a','b','c','d','e','f','g','h','i'];
-        foreach($options as $opt){
+        $options = ['question', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+        foreach ($options as $opt) {
             $text = $request->get($opt);
-            if(preg_match('/<img/', $text))
-                $text = summernote_imageupload($user,$text);
-            $request->merge([$opt=>$text]);
+
+            if (preg_match('/<img/', $text)) {
+                $text = summernote_imageupload($user, $text);
+            }
+            $request->merge([$opt => $text]);
         }
-        
     }
 
-    public function d($test_id,$id)
+    public function d($test_id, $id)
     {
-        $obj = Obj::where('id',$id)->first();
-    
+        $obj = Obj::where('id', $id)->first();
+
         $this->authorize('view', $obj);
 
-        $last = Obj::where('test_id',$test_id)->orderBy('id','desc')->first();
+        $last = Obj::where('test_id', $test_id)->orderBy('id', 'desc')->first();
         $str = substr(md5(time()), 0, 7);
         $f_new = $obj->replicate();
-        $f_new->sno = intval($last->sno)+1;
-        $f_new->qno = intval($last->qno)+1;
+        $f_new->sno = intval($last->sno) + 1;
+        $f_new->qno = intval($last->qno) + 1;
         $f_new->save();
 
 
-        if($obj)
-            return redirect()->route('mcq.index',$test_id);
+        if ($obj)
+            return redirect()->route('mcq.index', $test_id);
         else
             abort(404);
     }
@@ -220,19 +218,19 @@ class McqController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($test_id,$id)
+    public function show($test_id, $id)
     {
-        $obj = Obj::where('id',$id)->first();
+        $obj = Obj::where('id', $id)->first();
 
         $this->authorize('view', $obj);
-        if($obj)
-            return view('appl.'.$this->app.'.'.$this->module.'.show')
-                    ->with('obj',$obj)
-                    ->with('m',$obj)
-                    ->with('answers',null)
-                    ->with('app',$this)
-                    ->with('try',true)
-                    ->with('gre',1);
+        if ($obj)
+            return view('appl.' . $this->app . '.' . $this->module . '.show')
+                ->with('obj', $obj)
+                ->with('m', $obj)
+                ->with('answers', null)
+                ->with('app', $this)
+                ->with('try', true)
+                ->with('gre', 1);
         else
             abort(404);
     }
@@ -243,24 +241,24 @@ class McqController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($test_id,$id)
+    public function edit($test_id, $id)
     {
-        $obj= Obj::where('id',$id)->first();
+        $obj = Obj::where('id', $id)->first();
         $this->authorize('update', $obj);
 
-        $extracts = Extract::where('test_id',$this->test->id)->get();
-        $sections = Section::where('test_id',$this->test->id)->get();
-         $tags = Tag::all();
+        $extracts = Extract::where('test_id', $this->test->id)->get();
+        $sections = Section::where('test_id', $this->test->id)->get();
+        $tags = Tag::all();
 
-        if($obj)
-            return view('appl.'.$this->app.'.'.$this->module.'.createedit')
-                ->with('stub','Update')
-                ->with('obj',$obj)
-                ->with('editor',true)
-                ->with('extracts',$extracts)
-                ->with('sections',$sections)
-                ->with('tags',$tags)
-                ->with('app',$this);
+        if ($obj)
+            return view('appl.' . $this->app . '.' . $this->module . '.createedit')
+                ->with('stub', 'Update')
+                ->with('obj', $obj)
+                ->with('editor', true)
+                ->with('extracts', $extracts)
+                ->with('sections', $sections)
+                ->with('tags', $tags)
+                ->with('app', $this);
         else
             abort(404);
     }
@@ -272,68 +270,68 @@ class McqController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $test_id,$id)
+    public function update(Request $request, $test_id, $id)
     {
-        try{
-            $obj = Obj::where('id',$id)->first();
+        try {
+            $obj = Obj::where('id', $id)->first();
 
             $this->authorize('update', $obj);
 
             $user = \auth::user();
             $request->session()->put('extract_id', $request->get('extract_id'));
-            /* upload images if any */
-            $question = summernote_imageupload($user,$request->get('question'));
-            $a = summernote_imageupload($user,$request->get('a'));
-            $b = summernote_imageupload($user,$request->get('b'));
-            $c= summernote_imageupload($user,$request->get('c'));
-            $d= summernote_imageupload($user,$request->get('d'));
+            $this->imageupdater($request);
 
-            foreach(['a','b','c','d'] as $item){
-                if(trim($request->get($item))=='<p><br></p>')
-                    $request->merge([$item=>'']);
+            /* upload images if any */
+            // $question = summernote_imageupload($user, $request->get('question'));
+            // $a = summernote_imageupload($user, $request->get('a'));
+            // $b = summernote_imageupload($user, $request->get('b'));
+            // $c = summernote_imageupload($user, $request->get('c'));
+            // $d = summernote_imageupload($user, $request->get('d'));
+
+            foreach (['a', 'b', 'c', 'd'] as $item) {
+                if (trim($request->get($item)) == '<p><br></p>')
+                    $request->merge([$item => '']);
             }
-            
-            $request->merge(['qno'=>str_replace(" ","",$request->qno)]);
+
+            $request->merge(['qno' => str_replace(" ", "", $request->qno)]);
             // for multi answer
             $answers = $request->get('answers');
-            if($answers){
+            if ($answers) {
                 $answer = implode(', ', $answers);
                 /* merge the updated data in request */
-                $request->merge(['answer'=>$answer]);
-            }else{
-                $request->merge(['answer'=>'']);
+                $request->merge(['answer' => $answer]);
+            } else {
+                $request->merge(['answer' => '']);
             }
 
             $tags = $request->get('tags');
-            if($tags){
+            if ($tags) {
                 $obj->tags()->detach();
-                foreach($tags as $tag){
-                $obj->tags()->attach($tag);
+                foreach ($tags as $tag) {
+                    $obj->tags()->attach($tag);
                 }
-            }else{
+            } else {
                 $obj->tags()->detach();
             }
 
             //update extract
-            if($request->get('extract_id'))
-            {
-                if($obj->extract)
-                $obj->extract->extract_update($obj->qno);
+            if ($request->get('extract_id')) {
+                if ($obj->extract)
+                    $obj->extract->extract_update($obj->qno);
             }
 
-            
-            $obj = $obj->update($request->all()); 
 
-           
+            $obj = $obj->update($request->all());
 
-            flash('('.$this->app.'/'.$this->module.') item is updated!')->success();
-            return redirect()->route($this->module.'.show',[$test_id,$id]);
-        }
-        catch (QueryException $e){
-           $error_code = $e->errorInfo[1];
-            if($error_code == 1062){
-                 flash('Some error in updating the record')->error();
-                 return redirect()->back()->withInput();
+
+
+            flash('(' . $this->app . '/' . $this->module . ') item is updated!')->success();
+            return redirect()->route($this->module . '.show', [$test_id, $id]);
+        } catch (QueryException $e) {
+            $error_code = $e->errorInfo[1];
+            if ($error_code == 1062) {
+                flash('Some error in updating the record')->error();
+                return redirect()->back()->withInput();
             }
         }
     }
@@ -344,11 +342,11 @@ class McqController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($test_id,$id)
+    public function destroy($test_id, $id)
     {
-        $obj = Obj::where('id',$id)->first();
+        $obj = Obj::where('id', $id)->first();
         $this->authorize('update', $obj);
-        
+
         /* remove images before deleting */
         summernote_imageremove($obj->question);
         summernote_imageremove($obj->a);
@@ -358,7 +356,7 @@ class McqController extends Controller
 
         $obj->delete();
 
-        flash('('.$this->app.'/'.$this->module.') item  Successfully deleted!')->success();
-        return redirect()->route($this->module.'.index',$this->test->id);
+        flash('(' . $this->app . '/' . $this->module . ') item  Successfully deleted!')->success();
+        return redirect()->route($this->module . '.index', $this->test->id);
     }
 }
